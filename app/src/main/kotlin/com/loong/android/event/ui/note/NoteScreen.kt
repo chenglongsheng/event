e2cd4c0e728008796c8c80af4bb46f4e91d28927
loong <1536463948@qq.com>
2024-10-13 12:12:57 +0800

    feat: 处理软键盘和焦点

--- app/src/main/kotlin/com/loong/android/event/ui/note/NoteScreen.kt
+++ app/src/main/kotlin/com/loong/android/event/ui/note/NoteScreen.kt
@@ -2,7 +2,6 @@ package com.loong.android.event.ui.note
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,9 +10,11 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.automirrored.rounded.Redo
+import androidx.compose.material.icons.automirrored.rounded.Undo
+import androidx.compose.material.icons.rounded.Done
 import androidx.compose.material.icons.rounded.IosShare
 import androidx.compose.material.icons.rounded.MoreVert
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,25 +22,28 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.OnApplyWindowInsetsListener
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 @Composable
 fun NoteScreen(modifier: Modifier = Modifier, backAction: () -> Unit) {
@@ -50,87 +54,81 @@ fun NoteScreen(modifier: Modifier = Modifier, backAction: () -> Unit) {
 @Composable
 fun RichTextEditor(modifier: Modifier = Modifier, backAction: () -> Unit) {
     var text by remember { mutableStateOf(TextFieldValue(AnnotatedString(""))) }
-    var isBold by remember { mutableStateOf(false) }
-    var isItalic by remember { mutableStateOf(false) }
-    var isUnderline by remember { mutableStateOf(false) }
+    val focusRequester = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
+    var isFocused by remember { mutableStateOf(false) }
+
+    val view = LocalView.current
+    DisposableEffect(view) {
+        val listener = OnApplyWindowInsetsListener { _, insets ->
+            val isKeyboardVisible = insets.isVisible(WindowInsetsCompat.Type.ime())
+            if (!isKeyboardVisible) {
+                // 键盘关闭时清除焦点
+                focusManager.clearFocus()
+            }
+            insets
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(view, listener)
+
+        onDispose {
+            ViewCompat.setOnApplyWindowInsetsListener(view, null)
+        }
+    }
 
     Scaffold(topBar = {
         TopAppBar(title = {
             Text(text = "Rich Text Editor")
         }, navigationIcon = {
             IconButton(onClick = {
+                focusManager.clearFocus()
                 backAction()
             }) {
                 Icon(
                     imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
-                    contentDescription = "ArrowBack"
+                    contentDescription = "Back"
                 )
             }
         }, actions = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Rounded.IosShare, contentDescription = "Share")
+            if (isFocused) {
+                IconButton(onClick = { /*TODO*/ }, modifier = Modifier) {
+                    Icon(imageVector = Icons.AutoMirrored.Rounded.Undo, contentDescription = "Undo")
+                }
             }
             IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Rounded.MoreVert, contentDescription = "More")
+                val icon =
+                    if (isFocused) Icons.AutoMirrored.Rounded.Redo else Icons.Rounded.IosShare
+                val desc = if (isFocused) "Redo" else "Share"
+                Icon(imageVector = icon, contentDescription = desc)
+            }
+            IconButton(onClick = {
+                if (isFocused) {
+                    focusManager.clearFocus()
+                }
+            }) {
+                val icon = if (isFocused) Icons.Rounded.Done else Icons.Rounded.MoreVert
+                val desc = if (isFocused) "Done" else "More"
+                Icon(imageVector = icon, contentDescription = desc)
             }
         })
     }) { paddingValues ->
         Column(Modifier.padding(paddingValues)) {
-            // Buttons to toggle styles
-            Row(Modifier.padding(8.dp)) {
-                Button(onClick = { isBold = !isBold }) {
-                    Text(text = "Bold")
-                }
-                Button(onClick = { isItalic = !isItalic }) {
-                    Text(text = "Italic")
-                }
-                Button(onClick = { isUnderline = !isUnderline }) {
-                    Text(text = "Underline")
-                }
-            }
-
             BasicTextField(
                 value = text,
                 onValueChange = { newTextFieldValue ->
-                    text = newTextFieldValue.copy(
-                        annotatedString = AnnotatedString.Builder(newTextFieldValue.text).apply {
-                            // Apply styles based on button states
-                            if (isBold) {
-                                addStyle(
-                                    SpanStyle(fontWeight = FontWeight.Bold),
-                                    0,
-                                    newTextFieldValue.text.length
-                                )
-                            }
-                            if (isItalic) {
-                                addStyle(
-                                    SpanStyle(fontStyle = FontStyle.Italic),
-                                    0,
-                                    newTextFieldValue.text.length
-                                )
-                            }
-                            if (isUnderline) {
-                                addStyle(
-                                    SpanStyle(textDecoration = TextDecoration.Underline),
-                                    0,
-                                    newTextFieldValue.text.length
-                                )
-                            }
-                        }.toAnnotatedString()
-                    )
+                    text = newTextFieldValue
                 },
                 modifier = Modifier
+                    .focusRequester(focusRequester)
                     .fillMaxWidth()
                     .fillMaxHeight()
                     .onFocusChanged { focusState ->
-                        // Handle focus state if needed
+                        isFocused = focusState.isFocused
                     },
                 textStyle = TextStyle(color = Color.Black, fontSize = 18.sp),
                 decorationBox = { innerTextField ->
                     Box(
-                        modifier = modifier
-                            .padding(8.dp)
+                        modifier = modifier.padding(8.dp)
                     ) {
                         if (text.text.isEmpty()) {
                             Text("Enter rich text...", color = Color.Gray, fontSize = 18.sp)
