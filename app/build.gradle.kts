1b84804a53153bfbb15992f48c6a953b23cb504e
loong <1536463948@qq.com>
2025-01-21 23:02:40 +0800

    feat: 升级Sdk版本35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.loong.android.event"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.loong.android.event"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
